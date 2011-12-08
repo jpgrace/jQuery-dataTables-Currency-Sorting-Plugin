7aee995a1201c3f41e1fca2f49ce16c21d8cbb6f
JP Grace <jp@jpgrace.com>
2011-12-08 11:49:12 -0500

    first commit

---
+++ sorting.currency.js
@@ -0,0 +1,47 @@
+/*globals $ */
+$.fn.dataTableExt.oSort['currency-asc'] = function (a, b) {
+	'use strict';
+
+	var x, y;
+
+	/* Remove any commas (assumes that if present all strings will have a fixed number of d.p) */
+	x = (a === "-" || a === "--" || a === '' || a.toLowerCase().replace('/', '') === 'na') ? -1 : a.replace(/,/g, "");
+	y = (b === "-" || b === "--" || b === '' || b.toLowerCase().replace('/', '') === 'na') ? -1 : b.replace(/,/g, "");
+
+	/* Remove the currency sign */
+	if (typeof x === "string" && isNaN(parseInt(x.substring(1), 10))) {
+		x = x.substring(1);
+	}
+	if (typeof y === "string" && isNaN(parseInt(y.substring(1), 10))) {
+		y = y.substring(1);
+	}
+
+	/* Parse and return */
+	x = parseFloat(x, 10);
+	y = parseFloat(y, 10);
+
+	return x - y;
+};
+$.fn.dataTableExt.oSort['currency-desc'] = function (a, b) {
+	'use strict';
+
+	var x, y;
+
+	/* Remove any commas (assumes that if present all strings will have a fixed number of d.p) */
+	x = (a === "-" || a === "--" || a === '' || a.toLowerCase().replace('/', '') === 'na') ? -1 : a.replace(/,/g, "");
+	y = (b === "-" || b === "--" || b === '' || b.toLowerCase().replace('/', '') === 'na') ? -1 : b.replace(/,/g, "");
+
+	/* Remove the currency sign */
+	if (typeof x === "string" && isNaN(parseInt(x.substring(1), 10))) {
+		x = x.substring(1);
+	}
+	if (typeof y === "string" && isNaN(parseInt(y.substring(1), 10))) {
+		y = y.substring(1);
+	}
+
+	/* Parse and return */
+	x = parseFloat(x, 10);
+	y = parseFloat(y, 10);
+
+	return y - x;
+};
